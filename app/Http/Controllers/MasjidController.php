0d2133dfde2dea5166b8c9d42435ac1a325a50b8
avrians <avriansyahb10@gmail.com>
2023-07-03 15:16:17 +0700

    update tutor 6 2.15

--- app/Http/Controllers/MasjidController.php
+++ app/Http/Controllers/MasjidController.php
@@ -9,57 +9,20 @@ use App\Models\Masjid;
 class MasjidController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        $masjid = new Masjid();
+        return view('masjid_form', [
+            'masjid' => $masjid,
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreMasjidRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Masjid $masjid)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Masjid $masjid)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateMasjidRequest $request, Masjid $masjid)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Masjid $masjid)
     {
         //
     }
