b5ca764e8b3a7fb90c8e910bbe5da6a3bb5e2b15
avrians <avriansyahb10@gmail.com>
2023-08-10 21:57:20 +0700

    [Av][55][Done]-Event &Observer

--- app/Models/Kas.php
+++ app/Models/Kas.php
@@ -35,39 +35,4 @@ class Kas extends Model
         $masjid = Masjid::where('id', $masjidId)->first();
         return $masjid->saldo_akhir ?? 0;
     }
-
-    protected static function booted(): void
-    {
-        static::created(function (Kas $kas) {
-            $saldoAkhir = Kas::SaldoAkhir();
-            if ($kas->jenis == 'masuk') {
-                $saldoAkhir += $kas->jumlah;
-            } else {
-                $saldoAkhir -= $kas->jumlah;
-            }
-            $kas->masjid->update(['saldo_akhir' => $saldoAkhir]);
-        });
-
-        static::deleted(function (Kas $kas) {
-            $saldoAkhir = Kas::SaldoAkhir();
-            if ($kas->jenis == 'masuk') {
-                $saldoAkhir -= $kas->jumlah;
-            } else {
-                $saldoAkhir += $kas->jumlah;
-            }
-            $kas->masjid->update(['saldo_akhir' => $saldoAkhir]);
-        });
-
-        static::updated(function (Kas $kas) {
-            $saldoAkhir = Kas::SaldoAkhir();
-            if ($kas->jenis == 'masuk') {
-                $saldoAkhir -= $kas->getOriginal('jumlah');
-                $saldoAkhir += $kas->jumlah;
-            } else {
-                $saldoAkhir += $kas->getOriginal('jumlah');
-                $saldoAkhir -= $kas->jumlah;
-            }
-            $kas->masjid->update(['saldo_akhir' => $saldoAkhir]);
-        });
-    }
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Kas;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Observers\KasObserver;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,10 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+    ]; 
+
+    protected $observers = [
+        Kas::class => [KasObserver::class],
     ];
 
     /**
