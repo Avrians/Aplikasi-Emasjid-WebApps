532bae52a67a715e782b7d899e3077829da9a846
avrians <avriansyahb10@gmail.com>
2023-08-17 17:12:19 +0700

    [Av][64][Done]-Konfirmasi email

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use App\Http\Controllers\PesertaController;
 use App\Http\Controllers\ProfilController;
 use App\Http\Controllers\UserProfileController;
 use App\Http\Middleware\EnsureDataMasjidCompleted;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -40,7 +42,7 @@ Auth::routes();
 
 
 Route::middleware(['auth'])->group(function () {
-    Route::resource('masjid', MasjidController::class);
+    Route::resource('masjid', MasjidController::class)->middleware('verified');
 
     Route::middleware(EnsureDataMasjidCompleted::class)->group(function () {
         Route::resource('infaq', InfaqController::class);
@@ -52,8 +54,24 @@ Route::middleware(['auth'])->group(function () {
         Route::resource('informasi', InformasiController::class);
         Route::resource('kategori', KategoriController::class);
         Route::resource('profil', ProfilController::class);
-        Route::resource('kas', KasController::class); 
+        Route::resource('kas', KasController::class);
         Route::resource('userprofil', UserProfileController::class);
         Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     });
 });
+
+Route::get('/email/verify', function () {
+    return view('auth.verify');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.resend');
